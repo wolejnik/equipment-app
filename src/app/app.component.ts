5af64dbea957909d3d8c2e9ac6781cc19e3e3e96
w.olejnik@optimusway.io <w.olejnik@optimusway.io>
2020-06-18 18:00:11 +0200

    Get mockup data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Item } from './models/item';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'equipment-app';
+  itemsArray: Item[];
+  itemSelected: Item;
+  amountPrize: number = 0;
+
+  constructor(private items: DataService) { }
+
+  ngOnInit(): void {
+    this.getItems();
+    this.getPrizesValue();
+    this.getItem(1);
+    console.log('items', this.itemsArray, 'item', this.itemSelected, 'amountPrize', this.amountPrize);
+  }
+
+  getItem(id: number): void {
+    this.items.getItem(id).then(item => this.itemSelected = item);
+  }
+
+  getItems(): void {
+    this.items.getItems().then(items => this.itemsArray = items);
+  }
+
+  getPrizesValue(): void {
+    this.items.countAmountPrize().then(value => this.amountPrize = value);
+  }
+
+  selectItem(id: number) {
+    this.getItem(id);
+  }
 }
